3917a73075ccc36f5a06bd5199311ffaafcd6d6f
D'Janae Patterson <goldamber1412@gmail.com>
2021-11-15 15:18:36 -0800

    Added Java file

---
+++ swen.java
@@ -0,0 +1,22 @@
+swen.java
+import java.util.Scanner;
+
+public class Mileagw
+{
+    /*Testing 1,2,3*/
+    public static void main (String[] args)
+    {
+        int miles;
+        double gallons, mpg;
+        //mpg = miles/
+        Scanner s = new Scanner(system.in);
+
+        System.out.println("Enter miles and gallons: ");
+        miles = s.nextInt();
+        gallons = s.nextDouble();
+        mpg = miles /gallons;
+
+        //output
+        System.out.println("Miles" + "Per" + "Gallon:");
+    }
+}
